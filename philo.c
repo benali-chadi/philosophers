117464e6e59a64f1991de3c702a33ff577172a37
benali-chadi <benalioulhajchadi@gmail.com>
2021-07-11 21:52:56 +0100

    ld

--- philo.c
+++ philo.c
@@ -16,8 +16,11 @@ void	init_philos(t_options options, t_philo *philos)
 	while (++i < options.num_of_philos)
 	{
 		philos[i].num = i + 1;
-		pthread_create(philos[i].philo_t, NULL, &philo_routine, NULL);
+		pthread_create(&philos[i].philo_t, NULL, &philo_routine, NULL);
 	}
+    i = -1;
+    while (++i < options.num_of_philos)
+        pthread_join(philos[i].philo_t, NULL);
 }
 
 int main (int ac, char **av)
@@ -26,14 +29,14 @@ int main (int ac, char **av)
 	t_philo		*philos;
 	if (ac > 4)
 	{
-		options.num_of_philos = av[1];
-		philos = malloc(sizeof(t_philo) * av[1] + 1);
-		options.time_to_die = av[2];
-		options.time_to_eat = av[3];
-		options.time_to_sleep = av[4];
+		options.num_of_philos = atoi(av[1]);
+		philos = malloc(sizeof(t_philo) * atoi(av[1]) + 1);
+		options.time_to_die = atoi(av[2]);
+		options.time_to_eat = atoi(av[3]);
+		options.time_to_sleep = atoi(av[4]);
 		options.num_must_eat = 0;
-		if (av[5])
-			options.num_must_eat = av[5];
+		if (atoi(av[5]))
+			options.num_must_eat = atoi(av[5]);
 		init_philos(options, philos);
 	}
 	return (0);
